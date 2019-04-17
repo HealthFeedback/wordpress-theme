3e15f933a1a0222acf8bba01d11d0040198edca4
Manu <emvincent@nsup.org>
2019-04-17 17:33:16 +0200

    https

--- templates/footer.php
+++ templates/footer.php
@@ -12,12 +12,12 @@
 	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>COMMUNITY</h4>   
             <p class="small"><a href="/community/">Reviewers</a></p>
-            <p class="small"><a href="http://sciencefeedback.co/community-standards/">Community standards</a></p>
+            <p class="small"><a href="https://sciencefeedback.co/community-standards/">Community standards</a></p>
             <p class="small"><a href="/for-scientists/">Apply to become a reviewer</a></p>
 	  	</div>
 	  	<div class="col-sm-3 col-xs-6">
 	  		<h4>ORGANIZATION</h4>
-            <p class="small"><a href="http://sciencefeedback.co/about/">About</a></p>
+            <p class="small"><a href="https://sciencefeedback.co/about/">About</a></p>
             <p class="small"><a href="/process/">Our method to evaluate articles</a></p>
             <p class="small"><a href="/contact-us/">Submit an item to review</a></p>
             <!--  
