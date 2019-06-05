9bf64f39cdea0fe93139b72075fd61e49d5813d3
Manu <emvincent@nsup.org>
2019-06-05 19:55:36 +0200

    add IFCN footer

--- templates/content-feedbacks.php
+++ templates/content-feedbacks.php
@@ -134,13 +134,13 @@
         <p class="small">
             Published on: <?php echo get_the_date( 'd M Y' ); ?> &#124; Editor: <?php the_author_posts_link(); ?>
         </p>
-<!--
+
   <div class="bot-box">
       <div class="row spaceup1">
           <div class="col-sm-9">
       <p class="small ">
-<em>Climate Feedback is a non-partisan, non-profit organization dedicated to science education. Our reviews are crowdsourced directly from a community of scientists with relevant expertise. We strive to explain whether and why information is or is not consistent with the science and to help readers know which news to trust. <br />
-Please <a href="https://climatefeedback.org/contact-us/">get in touch</a> if you have any comment or think there is an important claim or article that would need to be reviewed.</em>
+<em>Health Feedback is a non-partisan, non-profit organization dedicated to science education. Our reviews are crowdsourced directly from a community of scientists with relevant expertise. We strive to explain whether and why information is or is not consistent with the science and to help readers know which news to trust. <br />
+Please <a href="https://healthfeedback.org/contact-us/">get in touch</a> if you have any comment or think there is an important claim or article that would need to be reviewed.</em>
       </p>
           </div>
           <div class="col-sm-3">
@@ -148,6 +148,6 @@ Please <a href="https://climatefeedback.org/contact-us/">get in touch</a> if you
           </div>
       </div>
   </div>
--->
+
 
 <?php endwhile; ?>
